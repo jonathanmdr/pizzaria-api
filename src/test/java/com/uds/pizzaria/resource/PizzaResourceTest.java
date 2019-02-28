cb140cc5897b29858ae57b29fd7dabd2ae75e471
Jonathan Henrique Medeiros <jonathan.medeiros@ciss.com.br>
2019-02-28 15:08:14 -0300

    Implementação inicial de testes unitários na PizzaResource.

--- src/test/java/com/uds/pizzaria/resource/PizzaResourceTest.java
+++ src/test/java/com/uds/pizzaria/resource/PizzaResourceTest.java
@@ -4,8 +4,11 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import com.uds.pizzaria.event.RecursoCriadoEvent;
+import com.uds.pizzaria.model.Pizza;
 import com.uds.pizzaria.repository.PizzaRepository;
 import com.uds.pizzaria.service.PizzaService;
+import javax.servlet.http.HttpServletResponse;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,6 +23,8 @@ public class PizzaResourceTest {
 
     private PizzaResource subject;
 
+    private static Long ID_TESTE;
+
     @Mock
     private PizzaRepository pizzaRepository;
 
@@ -32,6 +37,7 @@ public class PizzaResourceTest {
     @Before
     public void before() {
         subject = new PizzaResource(pizzaRepository, pizzaService, eventPublisher);
+        ID_TESTE = new Long(1);
     }
 
     @Test
@@ -41,6 +47,35 @@ public class PizzaResourceTest {
         subject.findAll(pageable);
         when(pizzaRepository.findAll(pageable)).thenReturn(mock(Page.class));
         verify(pizzaRepository).findAll(pageable);
+    }
+
+    @Test
+    public void findAllResumo() {
+        Pageable pageable = mock(Pageable.class);
+
+        subject.findAllResumo(pageable);
+        when(pizzaRepository.resumir(pageable)).thenReturn(mock(Page.class));
+        verify(pizzaRepository).resumir(pageable);
+    }
+
+    @Test
+    public void findById() {
+        subject.findById(ID_TESTE);
+        when(pizzaRepository.findOne(ID_TESTE)).thenReturn(mock(Pizza.class));
+        verify(pizzaRepository).findOne(ID_TESTE);
+    }
+
+    @Test
+    public void save() {
+        Pizza pizza = mock(Pizza.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+
+        pizzaService.save(pizza);
+
+        eventPublisher.publishEvent(new RecursoCriadoEvent(mock(PizzaResource.class), response, pizza.getId()));
+
+        when(pizzaService.save(pizza)).thenReturn(mock(Pizza.class));
+        verify(pizzaService).save(pizza);
     }
 
 }
